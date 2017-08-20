9012f5358bb86f6c762eb88ea7c5a88acb0a4fb7
yash98 <yashmalviya98@gmail.com>
2017-08-20 14:53:38 +0530

    20/08

---
+++ COL106/A1/A1/FabricBreakup.java
@@ -0,0 +1,12 @@
+public class FabricBreakup {
+    public int maxArrSize = 1;
+    public int[] arrOfShirts = new int[1];
+    public int rear = 0;
+    public int favShirtIndex = 0;
+
+    public void fold (int rating) {
+        try {
+            
+        }
+    }
+}
